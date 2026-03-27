e06b5df006554bf12dc0d9c08f25b4ed4595abcd
popo <144322354+yosita-rd@users.noreply.github.com>
2026-03-27 23:08:25 +0700

    refactor

--- src/features/settings/components/index.ts
+++ src/features/settings/components/index.ts
@@ -1,5 +1,6 @@
 export { DelegationForm } from './DelegationForm';
 export { DelegateEditor } from './DelegateEditor';
+export { DepartmentRepsManager } from './DepartmentRepsManager';
 export { InlineActionRow } from './InlineActionRow';
 export { UserSearchCombobox } from './UserSearchCombobox';
 export { WorkflowTags } from './WorkflowTags';
